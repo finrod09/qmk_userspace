8fb4c7ea73360f64d88454ee450b695cdb421f28
burkfers <finrod09@users.noreply.github.com>
2024-06-13 10:31:17 +0200

    add peacock

---
+++ keyboards/georgen/peacock/keymaps/burkfers/config.h
@@ -0,0 +1,7 @@
+// By default the sensor is tuned for a thin vinyl surface, to adjust for a 3mm acrylic surface set the following
+// #define MXT_SURFACE_TYPE ACRYLIC
+
+#undef RGB_MATRIX_DEFAULT_MODE
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#define RGB_MATRIX_DEFAULT_HUE 200
